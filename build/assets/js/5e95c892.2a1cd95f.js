d45856fcfcc46af58a70469f5b745f46e0259e70
alfonsín <ponchodeandan@gmail.com>
2024-08-02 20:31:45 -0600

    actualizacion de permisos}

---
+++ build/assets/js/5e95c892.2a1cd95f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcha_bots_docs=self.webpackChunkcha_bots_docs||[]).push([[9647],{7121:(s,e,c)=>{c.r(e),c.d(e,{default:()=>h});c(6540);var r=c(4164),a=c(1213),t=c(7559),u=c(2831),o=c(781),d=c(4848);function h(s){return(0,d.jsx)(a.e3,{className:(0,r.A)(t.G.wrapper.docsPages),children:(0,d.jsx)(o.A,{children:(0,u.v)(s.route.routes)})})}}}]);
